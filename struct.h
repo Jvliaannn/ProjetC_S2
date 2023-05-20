8cfe4ad8fac66c74bb1b588576206c63faea0c80
magickazer <91758375+magickazer@users.noreply.github.com>
2023-05-21 00:26:40 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -1,34 +1,37 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
-
 #include <stdbool.h>
+#include <string.h>
 
-typedef struct Node {             // Noeud d'un graphe
-                            
-    int vertex;
-    struct Node* next;
 
-} Node;
 
-typedef struct Graphe {
+typedef struct Noeud {
+    int sommet;
+    struct Noeud* suivant;
+} Noeud;
 
-    int numVertices;
-    Node** adjacencyList;
-
+// Structure de graphe
+typedef struct {
+    int sommetDeDepart;
+    int sommetArrive;
+    int Nbsommet;
+    int** MatriceAdjacente;
+    Noeud** listeSuccesseurs;
+    
 } Graph;
 
-// Initialisation du graphe avec un certain nombre de sommets
-Graph* createGraph(int numVertices) {
+void initializeGraph(Graph* graph);
+void addEdge(Graph* graph, int sommet, int successeurs);
+void LireGraphe(Graph* graph, FILE* fichier);
+void printAdjacencyMatrix(Graph* graph);
+void convertirMatriceEnListeSuccesseurs(Graph* graph);
+void printSuccessorList(Graph* graph);
+void parcoursProfondeur(Graph* graph, int sommet, bool* visite);
+void rechercheEnProfondeur(Graph* graph);
 
-    Graphe* graphe = (Graph*)malloc(sizeof(Graph));
-    graphe -> numVertices = numVertices;
-    graphe -> adjacencyList = (Node**)malloc(numVertices * sizeof(Node*));
 
-    for (int i = 0; i < numVertices; i++) {
 
-        graphe -> adjacencyList[i] = NULL;
-    }
-
-    return graphe;
-}
+#endif
