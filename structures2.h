43760f4fc55c7d0be47451058583a4af7374a7df
Shaekona <134173392+Shaekona@users.noreply.github.com>
2023-05-21 20:54:43 +0200

    Update structures2.h

--- structures2.h
+++ structures2.h
@@ -6,16 +6,19 @@
 #include <stdbool.h>
 #include <string.h>
 
+// Structure représentant un type de sommet
 typedef struct {
     int identifiant;
     char nom[20];
 } TypeSommet;
 
+// Structure représentant un noeud dans la liste des successeurs
 typedef struct Noeud {
     int sommet;
     struct Noeud* suivant;
 } Noeud;
 
+// Structure représentant un graphe
 typedef struct {
     int sommetDeDepart;
     int sommetArrive;
@@ -25,26 +28,29 @@ typedef struct {
     TypeSommet* types;
 } Graph;
 
+// Structure représentant les types de sommets d'un graphe
 typedef struct {
     TypeSommet* types;
     int nbTypes;
 } TypesGraphe;
 
+// Structure représentant les coûts des types de sommets
 typedef struct {
     char nom[20];
     double cout;
 } CoutType;
 
+// Structure représentant les coûts des types de sommets d'un graphe
 typedef struct {
     CoutType* couts;
     int nbCouts;
 } CoutsTypes;
 
+// Prototypes des fonctions
 TypesGraphe lireFichierTypeSommet(const char* nomFichier);
 CoutsTypes lireFichierType(const char* nomFichier);
 Graph lireFichierTerrain(const char* nomFichier);
 int trouverSommetMin(int distance[], int visite[], int n);
 void dijkstra(Graph graphe, int debut, int fin);
-
 
 #endif
